36d0c9b72835129e43869f773df885c3987f29be
JMarcelooo <jmarcelooocontato@gmail.comgit config --global user.email jmarcelooocontato@gmail.comgit config --global user.email jmarcelooocontato@gmail.com>
2025-05-02 21:17:51 -0300

    login screen

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import './Login.css';
+
+
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState(null);
+
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:3000/auth/login');
+            setToken(response.data.token);
+            alert('Login realizado com sucesso!');
+        } catch (error) {
+            alert('Erro no login:' + error.response.data.message);
+        }
+    };
+
+
+    return (
+        <div className='login-container'>
+            <h1>Login</h1>
+            <input type="email" placeholder='Email' onChange={e => setEmail(e.target)}></input>
+            <input type='password' placeholder='Senha' onChange={e => setPassword(e.target.value)}></input>
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    )
+}
+
+export default Login;
